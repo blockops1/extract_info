5cab0069bd8e73de05812b6a2616c4afa1d20de2
blockops1 <rolf@blockoperations.com>
2021-11-08 23:58:13 -0500

    ready to do zip

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class IterRegistry(type):
 class FoundFile(metaclass=IterRegistry):
 
     def __init__(self):
-        print("new FoundFile created")
+        # print("new FoundFile created")
         self.name = ""
         self.type = ""
         self.start = 0
@@ -38,8 +38,8 @@ def recover_file(file_handle, found_temp):
     hash_file.close()
     hash_sha256 = hashlib.sha256(hash_file_full).hexdigest()
     found_temp.hash = hash_sha256
-    # x = found_temp
-    # print("name: ", x.name, " type:", x.type, " start:", x.start, " end:", x.end, " size:", x.size, " hash:", x.hash)
+    x = found_temp
+    print("found a file - name:", x.name, "type:", x.type, "start:", x.start, "end:", x.end, "size:", x.size)
     return 0
 
 
@@ -128,15 +128,13 @@ def mpg(file_handle, position, temp_object):
             end_position_png = position_png
             break
         position_png += 1
-        if position_png % 10000000 == 0:
-            print(position_png)
     if end_position_png == 0:
         end_position_png = position_png + 4
     temp_object.end = end_position_png
     temp_object.size = temp_object.end - temp_object.start
-    x = temp_object
-    print("name: ", x.name, " type:", x.type, " start:", x.start, " end:", x.end, " size:", x.size, " hash:", x.hash)
-    print("calling recover file for mpg")
+    # x = temp_object
+    # print("name: ", x.name, " type:", x.type, " start:", x.start, " end:", x.end, " size:", x.size, " hash:", x.hash)
+    # print("calling recover file for mpg")
     time.sleep(5)
     recover_file(file_handle, temp_object)
     return 0
@@ -151,27 +149,44 @@ def pdf(file_handle, position, temp_object):
     end_position_png = 0
     # search for end of file
     current_bytes = ""
+    end_position_png = 0
     while position_png < file_size:
         file_handle.seek(position_png)
         current_bytes = bytes.hex(file_handle.read(7))
         if current_bytes == "0a2525454f460a" or current_bytes == "0d2525454f460d":
-            # end of file found
+            # possible end of file found
             end_position_png = position_png
-            break
+            # print("possible pdf ending string:", end_position_png)
         file_handle.seek(position_png)
         current_bytes = bytes.hex(file_handle.read(6))
         if current_bytes == "0a2525454f46":
             # end of file found
             end_position_png = position_png
+            # print("possible pdf ending string:", end_position_png)
+        # check for new file type signature
+        if end_position_png > 0 and current_bytes in specific_signatures:
+            # print("matched signature:", current_bytes, " for:", specific_signatures[current_bytes])
             break
         file_handle.seek(position_png)
         current_bytes = bytes.hex(file_handle.read(9))
         if current_bytes == "0d0a2525454f460d0a":
             # end of file found
             end_position_png = position_png
+            # print("possible pdf ending string:", end_position_png)
+        # now scan for start of a new filetype to find the real end of the pdf
+        file_handle.seek(position_png)
+        current_bytes = bytes.hex(file_handle.read(4))
+        if end_position_png > 0 and current_bytes in specific_signatures:
+            # print("matched signature:", current_bytes, " for:", specific_signatures[current_bytes])
+            break
+        # now scan for start of a new filetype to find the real end of the pdf
+        file_handle.seek(position_png)
+        current_bytes = bytes.hex(file_handle.read(8))
+        if end_position_png > 0 and current_bytes in specific_signatures:
+            # print("matched signature:", current_bytes, " for:", specific_signatures[current_bytes])
             break
         position_png += 1
-    print("pdf ending string:", current_bytes)
+    # print("pdf ending string position:", end_position_png)
     # time.sleep(5)
     temp_object.end = end_position_png
     temp_object.size = temp_object.end - temp_object.start
@@ -204,6 +219,26 @@ def gif(file_handle, position, temp_object):
     recover_file(file_handle, temp_object)
     return 0
 
+def bmp(file_handle, position, temp_object):
+    # print("position in function avi:", position)
+    file_handle.seek(position + 2)
+    temp_object.start = position
+    temp_object.size = int.from_bytes(file_handle.read(4), 'little')
+    # print("bmp size:", temp_object.size)
+    if temp_object.size > 1000000:
+        return 1
+    # create avi file object, recover file, and computes sha256
+    temp_object.name = str(temp_object.start) + ".bmp"
+    temp_object.end = temp_obj.start + temp_obj.size
+    temp_object.type = "bmp"
+    # print("calling recover file for avi")
+    # x = temp_object
+    # print("name: ", x.name, " type:", x.type, " start:", x.start, " end:", x.end, " size:", x.size, " hash:", x.hash)
+    recover_file(file_handle, temp_object)
+    file_handle.seek(position)
+    return 0
+
+
 def print_results():
     for x in found_files:
         print("name: ", x.name, " type:", x.type, " start:", x.start, " end:",  x.end, " size:", x.size, " hash:", x.hash)
@@ -218,7 +253,7 @@ if __name__ == '__main__':
         print("usage is python3 extract_info.py <filename>")
         exit(0)
     else:
-        print("analyzing file ", filename, " for file signatures")
+        print("analyzing file", filename, "for these file signatures:")
     found_files = []
     object_counter = 0
     # setup signature hash table
@@ -226,7 +261,7 @@ if __name__ == '__main__':
     first_byte_text = {'00': "mpg", "25": "pdf", "42": "bmp", "47": "gif", "ff": "jpg", "50": "zip",
                        "52": "avi", "89": "png"}
     # setup key/value number of bytes to read based on the first byte
-    number_of_bytes = {'mpg': 4, 'pdf': 4, 'bmp': 2, 'gif': 4, 'jpg': 4, 'zip': 4, 'avi': 4, 'png': 8}
+    number_of_bytes = {'mpg': 4, 'pdf': 4, 'bmp': 2, 'gif': 4, 'jpg': 6, 'zip': 4, 'avi': 4, 'png': 8}
     specific_signatures = {}
     # read signatures from file signature.txt
     try:
@@ -242,7 +277,8 @@ if __name__ == '__main__':
         if len(name) == 0:
             break
         specific_signatures[sig.rstrip()] = name.rstrip()
-    print(specific_signatures)
+    for x in specific_signatures:
+        print(specific_signatures[x], "with signature", x)
     # read the analysis file
     try:
         working_file = open(filename, 'rb')
@@ -257,6 +293,8 @@ if __name__ == '__main__':
         current_byte = working_file.read(1)
         # convert to integer
         # working_number = int.from_bytes(current_byte, 'big')
+        if position % 10000000 == 0:
+            print("checking file at offset:", position)
         # convert byte to a string
         working_text = bytes.hex(current_byte)
         if working_text in first_byte_text:
@@ -266,7 +304,7 @@ if __name__ == '__main__':
             test_signature = bytes.hex(working_file.read(read_amount))
             # now that the correct number of bytes read, try to match against file signatures
             if test_signature in specific_signatures:
-                print("offset:", position, " signature:", test_signature, " file type:", specific_signatures[test_signature])
+                # print("offset:", position, " signature:", test_signature, " file type:", specific_signatures[test_signature])
                 if specific_signatures[test_signature] == "avi":
                     # print("matched signature:", test_signature, " for:", specific_signatures[test_signature])
                     temp_obj = FoundFile()
@@ -304,11 +342,20 @@ if __name__ == '__main__':
                     position = temp_obj.end
                     found_files.append(temp_obj)
                     working_file.seek(position)
+                if specific_signatures[test_signature] == "bmp":
+                    temp_obj = FoundFile()
+                    status = bmp(working_file, position, temp_obj)
+                    if status == 0:
+                        position = temp_obj.end
+                        found_files.append(temp_obj)
+                        working_file.seek(position)
         position += 1
+        # ?? round two on pdf's?
+        # zip files
     # cleanup by closing the file
     working_file.close()
     print("printing results")
-    print(found_files)
+    # print(found_files)
     print_results()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
